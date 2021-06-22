25ae1cc6f603ce9f1a2b50c67f09347d9c6dec3b
yuto-takami <orca.y1322@gmail.com>
2021-06-22 13:41:52 +0000

    [update]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,47 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Books.all
   end
-  
-  def show
-  end
-  
+
   def create
+    @book = Book.new(book_params)
+    if @book.save
+       flash[:notice] = 'You have created book successfully.'
+       redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
-  
-  def update
+
+  def show
+    @book = Book.find(params[:id])
   end
-  
+
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       flash[:notice] = 'You have updated book successfully.'
+       redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
   def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to
+    end
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
