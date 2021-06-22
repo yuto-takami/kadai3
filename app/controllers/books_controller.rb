b16059b632affa32783b9b82b2872e93c3c5547f
yuto-takami <orca.y1322@gmail.com>
2021-06-22 08:32:19 +0000

    [update]kadai3

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,19 @@
+class BooksController < ApplicationController
+  def index
+  end
+  
+  def show
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def edit
+  end
+  
+  def destroy
+  end
+end
