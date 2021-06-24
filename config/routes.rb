955273f0de0452fce6342990d6e29822aa8d1a7e
yuto-takami <orca.y1322@gmail.com>
2021-06-24 18:17:44 +0000

    [update]kadai3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  
+  get 'home/about' => 'homes#index', as: 'homes'
+  
+  
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy]
 
   resources :users, only: [:index, :show, :edit, :update]
