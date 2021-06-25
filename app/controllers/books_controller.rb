acfb6668754df2dfdf764cc9b6b7a0124d636c39
yuto-takami <orca.y1322@gmail.com>
2021-06-25 07:54:01 +0000

    [update]kadai3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
  
  def show
    @book = Book.find(params[:id])
+   @books = Book.new
  end
  
  def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,20 @@ class UsersController < ApplicationController
   
   def index
     @users = User.all
+    
   end
   
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
   
   def edit
@@ -22,6 +31,10 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:name, :profile_image, :introduction)
+    end
+    
+    def book_params
+    params.require(:book).permit(:title, :body)
     end
 end
 
