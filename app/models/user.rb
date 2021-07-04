4d0bc7ef6950ef06afbdcea512549872573fb4ab
yuto-takami <orca.y1322@gmail.com>
2021-07-04 15:25:16 +0000

    [Start]ouyoukadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :xxx, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :yyy, through: :xxx, source: :zzz
+  has_many :xxx, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :yyy, through: :xxx, source: :zzz
   attachment :profile_image
   
   validates :name, length: {minimum:2, maximum:20}, uniqueness: true
