5c4816c099a73c4cb1b75d0c266a7f392dc50237
yuto-takami <orca.y1322@gmail.com>
2021-07-05 09:13:08 +0000

    [Finish]ouyoukadai4

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,29 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :xxx, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :yyy, through: :xxx, source: :zzz
-  has_many :xxx, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :yyy, through: :xxx, source: :zzz
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
   attachment :profile_image
-  
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    
+    followings.include?(user)
+  end
+
   validates :name, length: {minimum:2, maximum:20}, uniqueness: true
   validates :introduction, length: {maximum:50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   
   get 'home/about' => 'homes#about', as: 'homes'
   
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books, only: [:index, :create, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
