59135a40ce36d85f708e0aa0344093cbe8fe6759
yuto-takami <orca.y1322@gmail.com>
2021-07-06 13:00:09 +0000

    [Finish]ouyoukadai5

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'search/search'
   devise_for :users
   root to: 'homes#top'
-  
+
   get 'home/about' => 'homes#about', as: 'homes'
-  
+
   resources :users, only: [:index, :show, :edit, :update] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
@@ -14,4 +14,6 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  
+  get '/search' => 'search#search'
 end
