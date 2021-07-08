d3f622be343369c54bd17ea1d2d55b414dfa51d4
yuto-takami <orca.y1322@gmail.com>
2021-07-08 13:17:22 +0000

    [Finish]ouyoukadai6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,15 @@
 class BookCommentsController < ApplicationController
    def create
-     book = Book.find(params[:book_id])
-     comment = current_user.book_comments.new(book_comment_params)
-     comment.book_id = book.id
-     comment.save
-     redirect_to book_path(book)
+     @book = Book.find(params[:book_id])
+     @comment = current_user.book_comments.new(book_comment_params)
+     @comment.book_id = @book.id
+     @comment.save
+     
    end
    
    def destroy
-     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-     redirect_to book_path(params[:book_id])
+     @comment = BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+     
    end
    
    private
