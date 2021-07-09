806831ebac20d8a8643acce2e5cb6ff0e28513d6
yuto-takami <orca.y1322@gmail.com>
2021-07-09 05:16:14 +0000

    [Finish]ouyoukadai6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
-  
+
    def index
      @user = current_user
      @book = Book.new
-     @books = Book.all
+     @books = Book.includes(:favorited_users).sort {|a,b| b.favorited_users.size <=> a.favorited_users.size}
    end
 
    def create
@@ -26,7 +26,7 @@ class BooksController < ApplicationController
      @book_comment = BookComment.new
    end
 
-   
+
    def edit
      @book = Book.find(params[:id])
      if @book.user == current_user
@@ -35,7 +35,7 @@ class BooksController < ApplicationController
         redirect_to books_path
      end
    end
-   
+
 
    def update
      @book = Book.find(params[:id])
@@ -58,9 +58,9 @@ class BooksController < ApplicationController
     def book_params
       params.require(:book).permit(:title, :body)
     end
-   
+
     def user_params
       params.require(:user).permit(:name, :profile_image, :introduction)
     end
-    
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :book
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
